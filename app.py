63c02522a1981cf98ec4353a19fb024f79846e71
antony-pereira <145911983+antony-pereira@users.noreply.github.com>
2024-11-28 14:39:06 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,99 @@
+import streamlit as st
+from menu import menu
+from pages.login import login_page
+from pages.cadastro import cadastro_page
+import mysql.connector
+
+
+
+# Initialize st.session_state.role to None
+if "role" not in st.session_state:
+    st.session_state.role = None
+
+# Retrieve the role from Session State to initialize the widget
+st.session_state._role = st.session_state.role
+
+if 'logged_in' not in st.session_state:
+    st.session_state['logged_in'] = False
+
+# Controla o estado da página de cadastro
+if 'show_cadastro' not in st.session_state:
+    st.session_state['show_cadastro'] = False
+
+
+def logout():
+    st.session_state['logged_in'] = False
+    st.session_state['show_cadastro'] = False
+    st.session_state.role = None
+    st.rerun()
+
+def set_role():
+    # Callback function to save the role selection to Session State
+    st.session_state.role = st.session_state._role
+
+def get_db_connection(perfil):
+    if perfil == 'Aberto':
+        return mysql.connector.connect(
+            host=st.secrets["DB_HOST"],
+            user='user_aberto',
+            password='senha_aberto',  # Senha do usuário 'usuario_aberto'
+            db=st.secrets["DB_NAME"]
+        )
+    elif perfil == 'Gerencial':
+        return mysql.connector.connect(
+            host=st.secrets["DB_HOST"],
+            user='user_gerencial',
+            password='senha_gerencial',  # Senha do usuário 'usuario_gerencial'
+            db=st.secrets["DB_NAME"]
+        )
+    else:
+        raise ValueError("Perfil desconhecido")
+
+if not st.session_state['logged_in']:
+    if st.session_state['show_cadastro']:
+        cadastro_page()  # Mostra a página de cadastro
+    else:
+    # Selectbox to choose role
+        st.title("Login")
+            
+        # Formulário de login
+        with st.form("login_form"):
+                email = st.text_input("Email")
+                senha = st.text_input("Senha", type="password")
+                submit_button = st.form_submit_button("Entrar")
+            
+        if submit_button:
+                # Conectar ao banco de dados para autenticação usando credenciais padrão
+                conn = mysql.connector.connect(
+                    host=st.secrets["DB_HOST"],
+                    user=st.secrets["DB_USERNAME"],
+                    password=st.secrets["DB_PASSWORD"],
+                    port=st.secrets["DB_PORT"],
+                    db=st.secrets["DB_NAME"]
+                )
+                cursor = conn.cursor()
+                query = "SELECT * FROM usuario WHERE email = %s AND senha = %s"
+                cursor.execute(query, (email, senha))
+                result = cursor.fetchone()
+                conn.close()
+                
+                if result:
+                    # Usuário autenticado, armazena informações no session_state
+                    perfil = result[5]  # O perfil está na coluna PERFIL
+                    st.session_state['logged_in'] = True
+                    st.session_state['user_id'] = result[0]  # Guarda o ID do usuário logado
+                    st.session_state._role = perfil  # Guarda o perfil do usuário
+                    set_role()
+                    
+                    # Use a função get_db_connection com base no perfil
+                    conn = get_db_connection(perfil)
+                    st.session_state['db_connection'] = conn
+                    
+                    st.rerun()  # Recarrega a aplicação para mostrar as páginas protegidas
+                else:
+                    st.error("Email ou senha incorretos!")
+            
+        if st.button("Não tem uma conta? Cadastre-se aqui"):
+                st.session_state['show_cadastro'] = True
+   
+menu() # Render the dynamic menu!
---
+++ menu.py
@@ -0,0 +1,36 @@
+import streamlit as st
+
+
+def authenticated_menu():
+    # Show a navigation menu for authenticated users
+    st.sidebar.page_link("pages/total.py", label="Total de alunos, professores e turmas")
+    st.sidebar.page_link("pages/Agrupar.py", label="Agrupar pelo nivel de ensino")
+    st.sidebar.page_link("pages/CodPessoa.py", label="Cod Pessoa")
+    st.sidebar.page_link("pages/Turma.py", label="Todas as turmas por escola")
+    st.sidebar.page_link("pages/notasideb.py", label="Notas IDEB")
+    st.sidebar.page_link("pages/bookmark.py", label="Lista de escolas")
+    if st.session_state.role in ["Gerencial"]:
+        st.sidebar.page_link("pages/CRUD.py", label="Lista de Usuarios e Bookmarks")
+        
+
+
+def unauthenticated_menu():
+    # Show a navigation menu for unauthenticated users
+    st.sidebar.page_link("app.py", label="Log in")
+
+
+def menu():
+    # Determine if a user is logged in or not, then show the correct
+    # navigation menu
+    if "role" not in st.session_state or st.session_state.role is None:
+        unauthenticated_menu()
+        return
+    authenticated_menu()
+
+
+def menu_with_redirect():
+    # Redirect users to the main page if not logged in, otherwise continue to
+    # render the navigation menu
+    if "role" not in st.session_state or st.session_state.role is None:
+        st.switch_page("app.py")
+    menu()
